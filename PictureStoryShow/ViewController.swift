3205f2db9653909d39836910bbeb37e2abf9efbe
Tomonori Ueno <t.uepon@gmail.com>
2021-04-16 10:34:49 +0700

    Initial Commit

---
+++ PictureStoryShow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureStoryShow
+//
+//  Created by SEZAX development on 16/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
